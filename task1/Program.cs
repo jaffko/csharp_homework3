470a6cd1c9d502e3d9740be814cdbbbc87829543
Ildar <steel14@yandex.ru>
2022-09-08 22:49:58 +0500

    added isPalindrom task

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+void isPalindrom(int num)
+{
+    if (num / 1000 == (num % 10 * 10) + (num / 10 % 10)) Console.WriteLine("Да");
+    else Console.WriteLine("Нет");
+}
+
+Console.WriteLine("Введите пяти значное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+isPalindrom(num);
